8d268e14752ccbe3f9f76b0a497edabc5a1c451d
dariopa <dario.panzuto@gmail.com>
2019-08-06 09:27:17 +0200

    -

--- LoadData.py
+++ LoadData.py
@@ -6,37 +6,25 @@ CallFolder = 'Raw_Data'
 
 #########################################################
 # IMPORT, PREPROCESS AND STORE TRAINING DATA AS NUMPY ARRAY
-DataTrain = np.genfromtxt(os.path.join(CallFolder, 'Train_Data.csv'), delimiter=',')
-print(DataTrain.size)
-# DataTrain = np.delete(DataTrain, 0, 0)
-# DataTrain = np.delete(DataTrain, 0, 1)
+DataTrain = np.genfromtxt(os.path.join(CallFolder, 'Train_Data.csv'), delimiter=';')
+y_train = DataTrain[:,6]
+X_train = DataTrain[:,1:6]
 
-X_train = DataTrain[:, 1:]
-print(X_train.size)
-y_train = DataTrain[:, 0]
-
-# #########################################################
-# # PREPROCESSING DATA
-# def retain(X_data):
-#     X_data[:, 0:3] = 0.
-#     X_data[:, 15:20] = 0.
-#     return X_data
-# #########################################################
+#########################################################
 # # GENERATE FULL DATA
-# row, col = X_train.shape
-# X_train_full = np.full((row, 21), 0., dtype=np.float16)
-# X_train_full[:, 0:5] = X_train
-# X_train_full[:, 5:10] = X_train ** 2
-# X_train_full[:, 10:15] = np.exp(X_train)
-# X_train_full[:, 15:20] = np.cos(X_train)
-# X_train_full[:, 20:21] = 1.
+row, col = X_train.shape
+X_train_full = np.full((row, 21), 0., dtype=np.float64)
+X_train_full[:, 0:5] = X_train
+X_train_full[:, 5:10] = X_train ** 2
+X_train_full[:, 10:15] = np.sin(X_train)
+X_train_full[:, 15:20] = np.cos(X_train)
+X_train_full[:, 20:21] = 1.
 
-# X_train_full = retain(X_train_full)
-# print(X_train_full)
+print(X_train_full)
     
-# #########################################################
-# # STORE DATA AS NUMPY ARRAY
-# print('X_train:   ', X_train_full.shape, end=' ||  ')
-# print('y_train:   ', y_train.shape)
-# np.save('X_train.npy', X_train_full)
-# np.save('y_train.npy', y_train)
+#########################################################
+# STORE DATA AS NUMPY ARRAY
+print('X_train:   ', X_train_full.shape)
+print('y_train:   ', y_train.shape)
+np.save('X_train.npy', X_train_full)
+np.save('y_train.npy', y_train)
